e01542c0fca265820ac1cdecb8ed623804ff41bc
LoezAnya <camiloamejia@unisinu.edu.co>
2022-08-25 09:30:20 -0500

    all passed

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -185,13 +185,16 @@ function redondearNumero(num) {
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  var red=x = Math.round(num,1);
+  var red=x = Math.ceil(num);
   return red;
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
+
+  let numrm=Math.random()*1;
+  return numrm;
   
 }
 
@@ -200,32 +203,46 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+
+  if(numero>0){
+    return "Es positivo";
+  }else if(numero<0){
+  return "Es negativo";}
+
+  return false;
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  let newStr=str+"!";
+  return newStr;
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  let str=nombre+" "+apellido;
+  return str;
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+
+  let str="Hola "+nombre+"!";
+  return str;
   
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+  let are=alto*ancho;
+  return are;
   
 }
 
@@ -233,13 +250,16 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  let per=lado*4;
+  return per;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
+    let are=(base*altura)/2;
+    return are;
 
 }
 
@@ -248,6 +268,9 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
+
+  let dol=euro*1.20;
+  return dol;
   
 }
 
@@ -258,6 +281,18 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+
+  if(letra.length>1){
+    return "Dato incorrecto";
+    
+  }
+
+  if(letra == 'a' || letra =='e' || letra == 'i' || letra =='o' || letra =='u'){
+    return "Es vocal";
+  }else{
+    return "Dato incorrecto";
+  }
+
   
 }
 
